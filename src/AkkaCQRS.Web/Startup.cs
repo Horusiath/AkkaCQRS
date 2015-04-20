5d470ec5413562bf5c76dc8153711fdd6ed27bfe
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2015-04-20 20:45:47 +0300

    removed javascript mess

--- src/AkkaCQRS.Web/Startup.cs
+++ src/AkkaCQRS.Web/Startup.cs
@@ -1,7 +1,14 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
 using AkkaCQRS.Core;
 using AkkaCQRS.Web;
 using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Microsoft.Owin.StaticFiles.Infrastructure;
 using Owin;
 
 [assembly: OwinStartup(typeof(Startup))]
@@ -13,7 +20,29 @@ namespace AkkaCQRS.Web
         public void Configuration(IAppBuilder app)
         {
             Bootstrap.Initialize();
-            app.UseWebApi(new HttpConfiguration(new HttpRouteCollection("~/api")));
+
+            ConfigureStaticFiles(app);
+            ConfigureWebApi(app);
+        }
+
+        private void ConfigureStaticFiles(IAppBuilder app)
+        {
+            var publicPath = Path.Combine(HttpContext.Current.Server.MapPath("~/"), "public");
+            app.UseFileServer(new FileServerOptions
+            {
+                EnableDirectoryBrowsing = true,
+                FileSystem = new PhysicalFileSystem(publicPath)
+            });
+        }
+
+        private static void ConfigureWebApi(IAppBuilder app)
+        {
+            var httpConfig = new HttpConfiguration();
+            httpConfig.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional});
+            app.UseWebApi(httpConfig);
         }
     }
 }
